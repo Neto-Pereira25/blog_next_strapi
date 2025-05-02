f13fdb67e9578e81ca1ff7e8bc2a25c1d27319f2
Neto-Pereira25 <devneto203@gmail.com>
2025-05-02 01:38:39 -0300

    tipagem dos posts e SSG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>Olá mundo</h1>;
+import { PostData } from '../domain/posts/post';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch(
+    'https://blog-api-strapi.onrender.com/api/posts/?populate=*',
+    { cache: 'force-cache' }, // equivalente a getStaticProps
+  );
+  const jsonPosts = await posts.json();
+  return jsonPosts.data;
+};
+
+export default async function Home() {
+  const posts = await getPosts();
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.slug}>{post.title}</h2>
+      ))}
+    </div>
+  );
 }
