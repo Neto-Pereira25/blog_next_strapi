dabf34e48cf48397f737aad1d0b4491cb387f8da
Neto-Pereira25 <devneto203@gmail.com>
2025-05-05 16:57:59 -0300

    Resolvido tipagem do DynamicPostProps

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -3,19 +3,22 @@ import { Post } from '../../../containers/Post/page';
 import { getPost } from '../../../data/posts/get-post';
 
 type DynamicPostProps = {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 };
 
 export default async function DynamicPost({ params }: DynamicPostProps) {
-  const { slug } = await params;
+  try {
+    const { slug } = await params;
 
-  const posts = await getPost(slug);
+    const posts = await getPost(slug);
 
-  if (!posts || posts.length === 0) {
-    return notFound();
+    if (!posts || posts.length === 0) {
+      return notFound();
+    }
+    const post = posts[0];
+
+    return <Post post={post} />;
+  } catch (error) {
+    console.log('Deu erro:', error);
   }
-
-  const post = posts[0];
-
-  return <Post post={post} />;
 }
