f96438a75bd2cb04842ad0e4d5f55e6e5b6d603f
Neto-Pereira25 <devneto203@gmail.com>
2025-05-05 18:10:17 -0300

    Adicionando <Head /> nos containers

--- src/containers/HomePage/page.tsx
+++ src/containers/HomePage/page.tsx
@@ -1,9 +1,11 @@
+import Head from 'next/head';
 import { Footer } from '../../components/Footer/page';
 import { Header } from '../../components/Header/page';
 import { MainContainer } from '../../components/MainContainer/page';
 import { PostCard } from '../../components/PostCard/page';
 import { PostData } from '../../domain/posts/post';
 import { Container } from './styled';
+import { SITE_NAME } from '../../app/config/app-config';
 
 export type HomePageProps = {
   posts: PostData[];
@@ -12,6 +14,13 @@ export type HomePageProps = {
 export default function HomePage({ posts }: HomePageProps) {
   return (
     <>
+      <Head>
+        <title>{SITE_NAME}</title>
+        <meta
+          name="description"
+          content="Este é meu blog de tecnologia feito com Next, Disqus para comentários e Strapi para API"
+        />
+      </Head>
       <Header />
       <MainContainer>
         <Container>
--- src/containers/Post/page.tsx
+++ src/containers/Post/page.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Comments } from '../../components/Comments/page';
 import { Footer } from '../../components/Footer/page';
 import { Header } from '../../components/Header/page';
@@ -7,6 +8,8 @@ import { PostContainer } from '../../components/PostContainer/page';
 import { PostCover } from '../../components/PostCover/page';
 import { PostDetails } from '../../components/PostDetails/page';
 import { PostData } from '../../domain/posts/post';
+import { SITE_NAME } from '../../app/config/app-config';
+import { removeHtml } from '../../utils/remove-html';
 
 export type PostProps = {
   post: PostData;
@@ -15,6 +18,15 @@ export type PostProps = {
 export const Post = ({ post }: PostProps) => {
   return (
     <>
+      <Head>
+        <title>
+          {post.title} - {SITE_NAME}
+        </title>
+        <meta
+          name="description"
+          content={removeHtml(post.content).slice(0, 150)}
+        />
+      </Head>
       <Header />
       <MainContainer>
         <Heading>{post.title}</Heading>
