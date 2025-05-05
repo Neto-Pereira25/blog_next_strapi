84a4829081d42459a4c3127297859206e695aed9
Neto-Pereira25 <devneto203@gmail.com>
2025-05-05 17:30:56 -0300

    Adicionando url de produção

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://blog-api-strapi.onrender.com/api';
 export const POSTS_URL = `${API_URL}/posts/?populate=*`;
 
 export const SITE_NAME = 'José Neto';
-export const SITE_URL = process.env.SITE_URL || 'http://vcap.me:3000';
+export const SITE_URL = 'https://blog-next-com-strapi-neto.netlify.app';
