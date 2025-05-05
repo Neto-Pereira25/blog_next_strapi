69afc6dc308b8f990b55637f750f0e2808789c9b
Neto-Pereira25 <devneto203@gmail.com>
2025-05-05 18:31:14 -0300

    Adicionado <Head /> na home e nos posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from 'next';
 import HomePage from '../containers/HomePage/page';
 import { getAllPosts } from '../data/posts/get-all-posts';
+import { SITE_NAME } from './config/app-config';
+
+export const metadata: Metadata = {
+  title: SITE_NAME,
+  description:
+    'Este é meu blog de tecnologia feito com Next, Disqus para comentários e Strapi para API',
+};
 
 export default async function Home() {
   const posts = await getAllPosts(
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { notFound } from 'next/navigation';
 import { Post } from '../../../containers/Post/page';
 import { getPost } from '../../../data/posts/get-post';
+import { Metadata } from 'next';
+import { SITE_NAME } from '../../config/app-config';
+import { removeHtml } from '../../../utils/remove-html';
 
 type DynamicPostProps = {
   params: Promise<{ slug: string }>;
@@ -21,4 +24,17 @@ export default async function DynamicPost({ params }: DynamicPostProps) {
   } catch (error) {
     console.log('Deu erro:', error);
   }
+}
+
+export async function generateMetadata({
+  params,
+}: DynamicPostProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const posts = await getPost(slug);
+
+  return {
+    title: `${posts[0].title} - ${SITE_NAME}`,
+    description: `${removeHtml(posts[0].content)}`,
+  };
 }
