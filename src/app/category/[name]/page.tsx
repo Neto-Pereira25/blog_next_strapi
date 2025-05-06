ffe35c10c2247b9acffb88c819719e6f10e13e2b
Neto-Pereira25 <devneto203@gmail.com>
2025-05-06 15:16:30 -0300

    Atualizou a <Head /> das categorias

--- src/app/category/[name]/page.tsx
+++ src/app/category/[name]/page.tsx
@@ -1,5 +1,7 @@
+import { Metadata } from 'next';
 import HomePage from '../../../containers/HomePage/page';
 import { getAllPostsOfThisCategory } from '../../../data/posts/get-all-posts-of-this-category';
+import { SITE_NAME } from '../../config/app-config';
 
 export type CategoryProps = {
   params: Promise<{ name: string }>;
@@ -11,4 +13,15 @@ export default async function Category({ params }: CategoryProps) {
   const posts = await getAllPostsOfThisCategory(name);
 
   return <HomePage posts={posts} category={name} />;
+}
+
+export async function generateMetadata({
+  params,
+}: CategoryProps): Promise<Metadata> {
+  const { name } = await params;
+
+  return {
+    title: `${name} - ${SITE_NAME}`,
+    description: `Este são os posts da categoria: ${name}`,
+  };
 }
